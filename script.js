d7eef8dd688bd8eac92f44b720f7706c7ddb52bd
swefd <swefd>
2021-11-14 13:49:52 +0200

    BOT TRIGGER

---
+++ script.js
@@ -0,0 +1,7 @@
+url = "https://api.telegram.org/bot2119137348:AAEJ7smJYNKueB_Bis4Fff9E8xJxjjtgYOA/sendMessage?chat_id=378318978&text=TEST"
+
+const startRequest = new XMLHttpRequest();
+
+
+startRequest.open('GET', url);
+startRequest.send();
